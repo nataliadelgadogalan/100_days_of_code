734b5a3729db2a332e2789740b25637fce73897c
Natalia Delgado Galán <natadgc@hotmail.com>
2017-12-05 23:25:51 +0100

    date validation

---
+++ day_7/main.py
@@ -0,0 +1,83 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import webapp2
+
+form="""
+<form method="post">
+	What is your birthday?
+	<br>
+	<label> Month
+		<input type"text" name="month">
+	</label>
+	<label> Day
+		<input type"text" name="day">
+	</label>
+	<label> Year
+		<input type"text" name="year">
+	</label>
+	<div style="color: red">%(error)s</div>
+	<br>
+	<br>
+	<input type="submit">
+</form>
+"""
+
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+          
+def valid_month(month):
+    if month.capitalize() in months:
+        return month.capitalize()
+    else:
+        return None
+
+def valid_day(day):
+    if day and day.isdigit():
+        if 0 < int(day) < 32:
+            return int(day)
+
+def valid_year(year):
+    if year and year.isdigit():
+        if 1900 < int(year) <2020:
+            return int(year)
+
+class MainPage(webapp2.RequestHandler):
+	def write_form(self, error=""):
+		self.response.out.write(form % {"error": error})
+
+	def get(self):
+		self.write_form()
+
+	def post(self):
+		user_month = valid_month(self.request.get("month"))
+		user_day = valid_day(self.request.get("day"))
+		user_year = valid_year(self.request.get("year"))
+
+		if not (user_month and user_day and user_year):
+			self.write_form("Uups, that doesnt look like a valid date")
+		else:
+			self.response.out.write("Thanks!")
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
